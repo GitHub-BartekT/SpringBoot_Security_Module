d5b290eea5ffab11bd6d3804110578ddd600f992
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-05-23 19:50:11 +0200

    Start project

---
+++ src/main/java/pl/iseebugs/SecurityEmail/SecurityEmailApplication.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.SecurityEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/iseebugs/SecurityEmail/SecurityEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.SecurityEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
