8bae08ec8b488966d4755fe31993f7fd3bf70b55
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-05-24 16:17:20 +0200

    Document dotenv

--- src/main/java/pl/iseebugs/Security/SecurityApplication.java
+++ src/main/java/pl/iseebugs/Security/SecurityApplication.java
@@ -1,5 +1,6 @@
 package pl.iseebugs.Security;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
