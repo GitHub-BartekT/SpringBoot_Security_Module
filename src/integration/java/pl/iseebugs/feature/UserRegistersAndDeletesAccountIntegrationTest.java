122431a0df4a399f46e65d3a9202e5a89b01d678
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-05-28 00:51:07 +0200

    Add 1 test step

--- src/integration/java/pl/iseebugs/feature/UserRegistersAndDeletesAccountIntegrationTest.java
+++ src/integration/java/pl/iseebugs/feature/UserRegistersAndDeletesAccountIntegrationTest.java
@@ -1,7 +1,12 @@
 package pl.iseebugs.feature;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import pl.iseebugs.BaseIntegrationTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class UserRegistersAndDeletesAccountIntegrationTest extends BaseIntegrationTest {
@@ -10,10 +15,22 @@ class UserRegistersAndDeletesAccountIntegrationTest extends BaseIntegrationTest 
     void should_user_registers_and_deletes_account() throws Exception {
     //Step 1: User tried to get JWT token by requesting POST /auth/signin
     //with username='someTestUser', password='somePassword' and system returned UNAUTHORIZED
-
+        // given && then
+        ResultActions failedLoginRequest = mockMvc.perform(post("/api/auth/signIn")
+                .content("""
+                {
+                "username": "someTestUser",
+                "password": "somePassword"
+                }
+                """.trim())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+        // then
+        failedLoginRequest
+                .andExpect(status().isForbidden());
 
     //Step 2: User made GET /{some public endpoint} and system returned OK(200) with some public response
-
+    
 
     //Step 3: user made POST /api/auth/signup with username="someTestUser", password="someTestPassword"
     //and system registered user with status OK(200) and token="someToken"
