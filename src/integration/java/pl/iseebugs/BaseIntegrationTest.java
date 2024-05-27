aeaac4abc68e8aa62b2d41da35ef33172df2c539
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-05-28 00:09:22 +0200

    Start test

---
+++ src/integration/java/pl/iseebugs/BaseIntegrationTest.java
@@ -0,0 +1,49 @@
+package pl.iseebugs;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import pl.iseebugs.Security.SecurityApplication;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(classes = SecurityApplication.class)
+@ActiveProfiles("integration")
+@AutoConfigureMockMvc
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    public static final String WIRE_MOCK_HOST = "http://localhost";
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Container
+    public static final PostgreSQLContainer<?> postgresqlContainer
+            = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test");
+
+    @RegisterExtension
+    public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @PostConstruct
+    public void setup() {
+        System.setProperty("DB_URL", postgresqlContainer.getJdbcUrl());
+        System.setProperty("DB_USERNAME", postgresqlContainer.getUsername());
+        System.setProperty("DB_PASSWORD", postgresqlContainer.getPassword());
+        System.setProperty("wiremock.server.port", String.valueOf(wireMockServer.getPort()));
+    }
+}
---
+++ src/integration/java/pl/iseebugs/feature/UserRegistersAndDeletesAccountIntegrationTest.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.feature;
+
+import org.junit.jupiter.api.Test;
+import pl.iseebugs.BaseIntegrationTest;
+
+
+class UserRegistersAndDeletesAccountIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void should_user_registers_and_deletes_account() throws Exception {
+
+    }
+}
